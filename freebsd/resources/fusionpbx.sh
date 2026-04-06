5db4a3f2596b3bb64d6757523fbf9a0dfe7350fd
Norman <norman@nsinnovations.net>
2026-04-06 08:40:27 -0400

    update fusionpbx git repo location.

--- freebsd/resources/fusionpbx.sh
+++ freebsd/resources/fusionpbx.sh
@@ -27,5 +27,5 @@ mkdir -p /var/cache/fusionpbx
 chown -R www:www /var/cache/fusionpbx
 
 #get the source code
-git clone $branch https://github.com/fusionpbx/fusionpbx.git /usr/local/www/fusionpbx
+git clone $branch https://$GIT_EMAIL:$GIT_PASSWORD@git.nsinnovations.net/nsinnovations/fusionpbx.git/usr/local/www/fusionpbx
 chown -R www:www /usr/local/www/fusionpbx
